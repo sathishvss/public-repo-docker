af8d3696c2373c1d60b7435a43bc94c0d7ebf936
Shivananda <devopsccnaacademy@gmail.com>
2026-01-31 14:16:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+// Serve index.html at "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
